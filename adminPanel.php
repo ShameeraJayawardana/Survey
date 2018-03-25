a94f0b30c6ee46706335f7df335699f8eb764ea5
ShameeraJayawardana <shamee.suj@gmail.com>
2018-03-26 00:18:04 +0530

    change the title

--- adminPanel.php
+++ adminPanel.php
@@ -31,7 +31,7 @@ $result_set1 = mysqli_query($conn, $q1);
     <body class="fixed-nav sticky-footer bg-dark" id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-            <a class="navbar-brand" href="index.php">Survey Department</a>
+            <a class="navbar-brand" href="index.php">Online Document Management System</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
