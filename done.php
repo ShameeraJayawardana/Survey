d75965e6c5e2b49491b7bf27671ad8e744430988
Buwaneka Sudheera Thisarasinghe <sudheerabuwaneka@gmail.com>
2018-05-15 20:09:23 +0530

    Created the return doc, when document is rejected it's added to the return doc again

--- done.php
+++ done.php
@@ -57,11 +57,12 @@ $row_set1 = mysqli_query($conn, $sql1);
                 <div class="jumbotron">
                     <h5><?php echo $row1['name']; ?></h5>
                     <span class="float-left">
-                        Requests for <?php echo $row1['number']; ?><br>
+                        Returned <?php echo $row1['number']; ?><br>
                         <span class="small float-right text-muted"><?php echo $row1['time']; ?></span>
                     </span><br><br><br>
                     <span class="float-right">
-                        <a class="btn btn-success" href="adminAccept.php?id=<?php echo urlencode($row1['id']); ?>&name=<?php echo urlencode($row1['name']); ?>">ACCEPT</a>
+                        <a class="btn btn-success" href="adminAccept.php?id=<?php echo urlencode($row1['id']); ?>&name=<?php echo urlencode($row1['name']); ?>">CONFIRM</a>
+                        <a class="btn btn-danger" href="adminIgnore.php?id=<?php echo urlencode($row1['id']); ?>&name=<?php echo urlencode($row1['name']); ?>">IGNORE</a>
                     </span>
                 </div>
             <?php } ?>
